7777e8928b7b46afaf302c459febe8241bc47671
Gabor Meresz <gabormeresz84@gmail.com>
2024-11-23 15:39:54 +0100

    Implementing SequentialChain

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--language", default="python")
@@ -14,18 +14,40 @@ load_dotenv()
 llm = OpenAI()
 
 code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}",
-    input_variables=["language","task"]
+    input_variables=["language","task"],
+    template="Write a very short {language} function that will {task}"
+)
+
+test_prompt = PromptTemplate(
+    input_variables=["language","code"],
+    template="Write a test for the following {language} code:\n{code}"
 )
 
 code_chain = LLMChain(
     llm=llm,
-    prompt=code_prompt
+    prompt=code_prompt,
+    output_key='code'
 )
 
-result = code_chain({
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key='test'
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language","task"],
+    output_variables=["test","code"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result["text"])
+print(">>>>>> GENERATED CODE:")
+print(result["code"])
+
+print(">>>>>> GENERATED TEST:")
+print(result["test"])
